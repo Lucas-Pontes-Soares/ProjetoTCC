61d344f1076871c9807aee0f89cd6edb5968f98f
Lucas <lucas.soares177@etec.sp.gov.br>
2023-02-23 11:27:25 -0300

    Xbox | getUserAchievements trazendo as imagens do jogo

--- source/controllers/xbox/getUserAchievements.js
+++ source/controllers/xbox/getUserAchievements.js
@@ -1,16 +1,30 @@
+const { trusted } = require('mongoose')
 var XboxApiClient = require('xbox-webapi')
 
 module.exports = getAchievementsProfile = async(req, res) => {
     req.client.isAuthenticated().then(function (){
         console.log('User is authenticated.')
+
+        const games = []
         //getTitleAchievements(TokenParaContinuar)
         req.client.getProvider('achievements').getTitleAchievements(0).then(async function(result){
-            console.log('resolve', result)
-            res.json({achievements: result}) 
+            Promise.all(result.titles.map(async(game)=>{
+                //console.log('resolve', game.titleId) = ids dos jogos
+                //buscar imagens por cada id de jogo
+                return req.client.getProvider('titlehub').getTitleId(game.titleId).then(function(titleDetails){
+                    game.image = titleDetails.titles[0].displayImage;
+                    //console.log(game)
+                    games.push(game)
+                    //titleDetails.titles[0].image = titleDetails.titles[0].displayImage
+                }).catch(function(error){
+                    res.json({success: false, message: "Imagem do jogo não encontrada", err: err.message})
+                }) 
+            })).then(()=>{
+                res.json({sucess: true, message: games})
+            }) 
         }).catch(function(error){
-            console.log('reject', error)
+            res.json({success: false, message: "Jogos não encontrados", err: err.message})
         })
-    
     }).catch(function(error){
         console.log('User is not authenticated. Run authentication flow first.', error)
     })
