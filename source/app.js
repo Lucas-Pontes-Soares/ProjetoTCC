3e887c9104b7ca88b321d7ba8d28816fd2cde611
Lucas <lucas.soares177@etec.sp.gov.br>
2023-02-27 11:28:21 -0300

    Adicionando CORS

--- source/app.js
+++ source/app.js
@@ -8,11 +8,13 @@ const playstationRouter = require("./routes/playstationRouter")
 const userRouter = require("./routes/userRouter")
 const PlayerRequestRouter = require("./routes/playerRequestRouter")
 var bodyParser = require("body-parser")
+var cors = require("cors");
 
 app.get('/', function (req, res) {
   res.send('Hello!');
 });
 
+app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(steamRouter)
