b787fc686e64b9fe01ba40b5be9b3f41637b53ca
Lucas <lucas.soares177@etec.sp.gov.br>
2023-03-06 11:33:53 -0300

    Controller createUser verificando se email existe

--- source/controllers/user/createUser.js
+++ source/controllers/user/createUser.js
@@ -1,10 +1,17 @@
 const userModel = require("../../database/models/usuario")
 
 module.exports = createUser = async(req, res) => {
+    const email = req.body.email
     const body = req.body
-    userModel.create(body).then((users)=>{
-        res.json({success: true, message: "Usuario criado", user: users})
-    }).catch((err)=>{
-        res.json({success: false, message: "Usuario não criado", err: err.message})
+    userModel.findOne({email: email}).then((user)=>{
+        if(!user){
+            userModel.create(body).then((users)=>{
+                res.json({success: true, message: "Usuario criado", user: users})
+            }).catch((err)=>{
+                res.json({success: false, message: "Usuario não criado", err: err.message})
+            })
+        } else {
+            res.json({success: false, message: "Usuario não criado, esse email ja está em uso"})
+        }
     })
 }
