fc658d651c0bb8d50e9065f7d3ea445043616a51
Lucas <lucas.soares177@etec.sp.gov.br>
2023-07-24 11:03:38 -0300

    campo last-name não é mais required na model usuario

--- source/database/models/usuario.js
+++ source/database/models/usuario.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 var user = new mongoose.Schema({
     first_name: {type: String, required: true},
-    last_name: {type: String, required: true},
+    last_name: {type: String, required: false},
     nick: {type: String, required: true},
     image: {type: String, required: false},
     email: {type: String, required: true},
