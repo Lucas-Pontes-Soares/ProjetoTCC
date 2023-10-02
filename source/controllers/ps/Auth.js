171f9aefecd8bb047eff1699f52d6686a2144839
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-10-02 09:32:33 -0300

    Atualizando myNpsso após expirar e verificando exceções steam

--- source/controllers/ps/Auth.js
+++ source/controllers/ps/Auth.js
@@ -3,9 +3,29 @@ const {exchangeCodeForAccessToken} = require("psn-api")
 
 module.exports = AuthenticationPlaystation = async(req, res, next) => {
     try {
-        const myNpsso = "7E6IDO86dVmBfUbswOmHyW8GDNSJk645YkQFNHH2EWKr5KoFv6cyqW87leeWbWvf";
+        const myNpsso = "T9VDd7nTKCnjwTlYoYyCgUgZeXS7vjWZDrzyIzRi1dyo7ZjzRP4719j9jCgLKVsp";
         const accessCode = await exchangeNpssoForCode(myNpsso);
         req.authorization = await exchangeCodeForAccessToken(accessCode);
+
+        const now = new Date();
+        const expirationDate = new Date(
+        now.getTime() + req.authorization.expiresIn * 1000
+        ).toISOString();
+
+        // ... some time later ...
+
+        // Since `expirationDate` is already an ISO date string,
+        // doing a comparison to see if it's expired is a one-liner.
+        const isAccessTokenExpired = new Date(expirationDate).getTime() < now.getTime();
+
+        if (isAccessTokenExpired) {
+        // We'll use our refresh token to get a new access token.
+        // Assuming success, this function returns an auth object
+        // with the same shape as the response from `exchangeCodeForAccessToken()`.
+        const updatedAuthorization = await exchangeRefreshTokenForAuthTokens(
+            req.authorization.refreshToken
+        );
+        }
         next()
     }catch(error){
         console.log(error)
--- source/controllers/steam/getPlayerGames.js
+++ source/controllers/steam/getPlayerGames.js
@@ -6,37 +6,51 @@ module.exports = getPlayerGames = async(req, res) => {
         let dados = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=02EF230DE713B2F6607714BFC2F58B69&steamid=${req.params.UserId}`) 
         dados = await dados.json()
 
+        //dados.response.games_count
         let gamePopulate = {}
-        Promise.all(dados.response.games.map(async(game)=>{
 
+        //variaveis para controlar a exibição apenas de 100 jogos por vez
+
+        //console.log(dados)
+        Promise.all(dados.response.games.filter((numeroJogoAtual, idx) => idx < 100).map(async(game)=>{
             //api para buscar nome e imagem do jogo
-            let dataGame = await fetch(`https://store.steampowered.com/api/appdetails?appids=${game.appid}`) 
-            if(dataGame){
-                dataGame = await dataGame.json()
-            }
-
-            //api para buscar as conquistas do jogo
-            let achivementGame = await fetch(`https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${game.appid}&key=02EF230DE713B2F6607714BFC2F58B69&steamid=${req.params.UserId}}`) 
-            achivementGame = await achivementGame.json()
-
-            //verifica se é um jogo, ou uma dlc que não possui dados
-            if(dataGame[game.appid].success == "false"){
-                console.log("Erro!")
-            }else{
-                gamePopulate = {
-                    appid: game.appid,
-                    title: dataGame[game.appid]?.data?.name  || "",
-                    image: dataGame[game.appid]?.data?.header_image || "",
+            
+            try {
+                let dataGame = await fetch(`https://store.steampowered.com/api/appdetails?appids=${game.appid}`);
+                if (dataGame.status === 200) {
+                  dataGame = await dataGame.json();
+                  //console.log(dataGame);
+                  
+                  // Se você chegou até aqui, os dados da API são válidos, e você pode continuar o processamento
+                  let achivementGame = await fetch(`https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${game.appid}&key=02EF230DE713B2F6607714BFC2F58B69&steamid=${req.params.UserId}`);
+                  achivementGame = await achivementGame.json();
+                  
+                if(dataGame[game.appid].length == 0 ||  dataGame[game.appid].success == "false"){
+                    console.log("Erro!")
+                }else{
+                        gamePopulate = {
+                            appid: game.appid,
+                            title: achivementGame.playerstats.gameName  || "",
+                            image: dataGame[game.appid]?.data?.header_image || "",
+                        }
+                        gameAchivements = {
+                            achivement: achivementGame.playerstats.achievements,
+                        }
+                        game.gameInfo = gamePopulate
+                        game.gameAchivement = gameAchivements 
+                    
                 }
-                gameAchivements = {
-                    achivement: achivementGame.playerstats.achievements,
+                } else {
+                  console.log("Erro ao buscar dados do jogo:", dataGame.status);
+                  return
                 }
-                game.gameInfo = gamePopulate
-                game.gameAchivement = gameAchivements
-            }
+              } catch (error) {
+                console.error("Erro ao buscar dados do jogo:", error.message);
+                return
+              }
 
         })).then(()=>{
-            res.json({games: dados.response.games}) 
+            res.json({games: dados.response.games})
         })
     }catch(err){
         console.log(err)
