2cc1837fe859f5f5e0b8dc815dd80cd996275346
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-10-06 11:45:30 -0300

    Atualizando CORS

--- source/app.js
+++ source/app.js
@@ -12,25 +12,21 @@ var cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 
+app.use(cors({ origin: `${process.env.URLFrontend}` }));
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(steamRouter);
+app.use(xboxRouter);
+app.use(playstationRouter);
+app.use(userRouter);
+app.use(PlayerRequestRouter);
+
 app.get('/', function (req, res) {
   res.send('Hello!');
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.URLFrontend}`);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
-  res.setHeader('Access-Control-Allow-Credentials', true); 
 });
 
-app.use(cors({origin: `${process.env.URLFrontend}`}));
-
-app.use(express.json())
-app.use(bodyParser.json());
-app.use(steamRouter)
-app.use(xboxRouter)
-app.use(playstationRouter)
-app.use(userRouter)
-app.use(PlayerRequestRouter)
-  
 app.listen(port, () => {
-  console.log(`App node rodando em: http://localhost:${port}`)
-  connect()
-})
+  console.log(`App node rodando em: http://localhost:${port}`);
+  connect();
+});
