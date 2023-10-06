416b63c3e19cb3907ab8c64354a1b4b9cd6aece6
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-10-06 20:43:35 -0300

    cors

--- source/app.js
+++ source/app.js
@@ -12,7 +12,13 @@ var cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use(cors({ origin: `${process.env.URLFrontend}` }));
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use(bodyParser.json());
