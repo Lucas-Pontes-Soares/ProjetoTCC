1380f3150bcd271cc48ed7274c8046b4cc27b494
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-10-06 21:03:31 -0300

    .

--- source/app.js
+++ source/app.js
@@ -20,6 +20,11 @@ const corsOptions ={
 
 console.log(corsOptions)
 app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+});
 
 app.use(express.json());
 app.use(bodyParser.json());
